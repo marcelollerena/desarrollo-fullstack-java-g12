cd6c730ade6aab4005c6c73347ccf3364f4b32a7
Marcelo <marcelo@Marcelos-MacBook-Pro.local>
2025-12-10 21:20:20 -0500

    add products route

--- 6-mini-ecommerce/src/router/router.tsx
+++ 6-mini-ecommerce/src/router/router.tsx
@@ -1,9 +1,14 @@
 import HomeScreen from "@/app/home/home";
+import ProductsScreen from "@/app/products/products";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeScreen />,
+  },
+  {
+    path: "/products",
+    element: <ProductsScreen />,
   },
 ]);
